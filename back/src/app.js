153c63ba1d25eae5e75ce101a8aefabcca982eca
ARTHUR FRANCISCO WERLANG LIMA <03542846066@senacrs.edu.br>
2024-11-12 09:52:45 -0300

    swagger

--- back/src/app.js
+++ back/src/app.js
@@ -8,9 +8,26 @@ const cors = require('cors');// Responsável por lidar com requisições externa
 const router = require('./routes/materialRouter');// Importar as rotas para serem executadas na aplicação
 
 const dotenv = require('dotenv').config();// Importar o pacote dotenv, gerenciador de variáveis de ambiente
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc")
 
+const swaggerOptions = {
+    swaggerDefinition: {
+      openapi: "3.0.0",
+      info: {
+        title: "API Reeco",
+        version: "1.0.0",
+        description: "API CRUD para gerenciar rotas de dados de endereços",
+      },
+      servers: [{ url: "http://localhost:3006" }],
+    },
+    apis: [`${__dirname}/routes/*.js`],
+  };
 
 const app = express();
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 
 app.use(express.json());// Habilitar o recebimento de requests em formato JSON
--- back/src/routes/materialRouter.js
+++ back/src/routes/materialRouter.js
@@ -5,6 +5,21 @@ const router = require('express').Router();
 
 const { getLocais } = require("../controllers/materialController");
 
+/**
+ * @swagger
+ * /locais/:
+ *  post:
+ *    summary: Busca os endereços na api
+ *    responses:
+ *      201:
+ *        description: Select nos endereços
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ */
 router.post('/locais/', getLocais);
 
 module.exports = router;
